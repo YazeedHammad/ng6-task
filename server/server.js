dbc09432832e0102f9735d2877cac64ab9ae66fe
yazeedhammad <yazeedhammad91@gmail.com>
2019-03-07 17:22:53 +0200

    deploy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 const api = require('./routes/api');
